cbdcd4dd08eb3847f666ba134d5136e551f33b22
YangTing <2428610923@qq.com>
2025-08-09 15:33:51 +0800

    first commit

---
+++ services/service-order/src/main/java/com/yang/order/OrderMainApplication.java
@@ -0,0 +1,16 @@
+package com.yang.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Description: TODO
+ * @Author: YangTing
+ * @Date: 2025/8/9 上午10:57
+ **/
+@SpringBootApplication
+public class OrderMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+}
---
+++ services/service-product/src/main/java/com/yang/product/ProductMainApplication.java
@@ -0,0 +1,16 @@
+package com.yang.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Description: TODO
+ * @Author: YangTing
+ * @Date: 2025/8/9 上午11:07
+ **/
+@SpringBootApplication
+public class ProductMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApplication.class, args);
+    }
+}
